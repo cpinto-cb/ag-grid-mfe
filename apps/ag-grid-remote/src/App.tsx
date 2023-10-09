d0d001afeb2d5ba9f682e71893e56a5831055954
ChrisP <cpinto@transperfect.com>
2023-10-09 10:54:45 -0400

    first commit with AG Grid example working.

---
+++ apps/ag-grid-remote/src/App.tsx
@@ -0,0 +1,12 @@
+import "ag-grid-enterprise";
+import Grid from "./Grid";
+import "./styles.css";
+
+export default function App() {
+    return (
+        <div>
+            <h2>AgGrid Example</h2>
+            <Grid />
+        </div>
+    );
+}
